012f6489ebe3e5b27bcc30dc1537abcd7c8a9bfa
Na <lengocanh2k@gmail.com>
2023-05-03 17:33:08 +0700

    init commit

---
+++ src/main/java/bkdn/afoodbe/AfoodBeApplication.java
@@ -0,0 +1,13 @@
+package bkdn.afoodbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfoodBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfoodBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bkdn/afoodbe/AfoodBeApplicationTests.java
@@ -0,0 +1,13 @@
+package bkdn.afoodbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfoodBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
