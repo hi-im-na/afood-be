e551710a88ce0991cfaf4cffa0035eb48f8447ca
Na <lengocanh2k@gmail.com>
2023-05-12 10:21:52 +0700

    delete StaffInfoRepository.java

--- src/main/java/bkdn/afoodbe/repository/StaffInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package bkdn.afoodbe.repository;
-
-import bkdn.afoodbe.entity.StaffInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StaffInfoRepository extends JpaRepository<StaffInfo, Integer> {
-}
