0126b881b9fe107cd13eccd118e85e13d812d18c
Na <lengocanh2k@gmail.com>
2023-05-12 10:20:51 +0700

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,14 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-rest")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("com.mysql:mysql-connector-j:8.0.33")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+    compileOnly("org.projectlombok:lombok:1.18.26")
 }
 
 tasks.withType<Test> {
