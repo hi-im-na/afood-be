acf91317405afae88d81cde0c24a71aa202c0f3c
Na <lengocanh2k@gmail.com>
2023-06-19 20:19:40 +0700

    change order out time no nullable

--- src/main/java/bkdn/afoodbe/entity/Order.java
+++ src/main/java/bkdn/afoodbe/entity/Order.java
@@ -1,8 +1,7 @@
 package bkdn.afoodbe.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.Instant;
 import java.util.LinkedHashSet;
@@ -10,8 +9,11 @@ import java.util.Set;
 
 @Getter
 @Setter
+@Builder
 @Entity
 @Table(name = "`order`")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,7 +23,7 @@ public class Order {
     @Column(name = "order_in_time", nullable = false)
     private Instant orderInTime;
 
-    @Column(name = "order_out_time", nullable = false)
+    @Column(name = "order_out_time", nullable = true)
     private Instant orderOutTime;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
